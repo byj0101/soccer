82bd378ccb1f01ba9c56e55e1034e97305da726d
BackYoungJae <byj010101@gmail.com>
2021-08-21 13:36:37 +0900

    Add initial style directory

---
+++ app/styles/home.ts
@@ -0,0 +1,17 @@
+// global import
+import {StyleSheet} from 'react-native';
+
+export const homeStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#dfdfdf',
+    flexDirection: 'row',
+    margin: 5,
+    padding: 5,
+    justifyContent: 'space-around',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+});
---
+++ app/styles/index.ts
@@ -0,0 +1,41 @@
+// global import
+import {StyleSheet} from 'react-native';
+
+export const defaultStyle = StyleSheet.create({
+  // font
+  fwb: {fontWeight: 'bold'},
+
+  // flex
+  fl0: {flex: 0},
+  fl05: {flex: 0.5},
+  fl1: {flex: 1},
+  fl2: {flex: 2},
+  fl3: {flex: 3},
+  flg0: {flexGrow: 0},
+  fls0: {flexShrink: 0},
+  flg1: {flexGrow: 1},
+  flg2: {flexGrow: 2},
+  fls1: {flexShrink: 1},
+  flb0: {flexBasis: 0},
+  flb100: {flexBasis: 100},
+  flb120: {flexBasis: 120},
+  flac: {alignItems: 'center'},
+  flafs: {alignItems: 'flex-start'},
+  flacfe: {alignItems: 'flex-end'},
+  flacsb: {alignContent: 'stretch'},
+  flasc: {alignSelf: 'center'},
+  flacc: {alignContent: 'center'},
+  fljfe: {justifyContent: 'flex-end'},
+  fljfs: {justifyContent: 'flex-start'},
+  fljc: {justifyContent: 'center'},
+  fljcsb: {justifyContent: 'space-between'},
+  fljcsa: {justifyContent: 'space-around'},
+  fldr: {flexDirection: 'row'},
+  fldc: {flexDirection: 'column'},
+  flw: {flexWrap: 'wrap'},
+  fljsb: {justifyContent: 'space-between'},
+
+  //opacity
+  op50: {opacity: 0.5},
+  op75: {opacity: 0.75},
+});
